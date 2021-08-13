1613d2abe2981d90ff1f0bbaf24c9a1c0e8f18e8
CristianJLopez <cjlopez60@misena.edu.co>
2021-08-12 20:15:39 -0500

    Create Project v1.0

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="../img/fav.png">
+    <!-- Author Meta -->
+    <meta name="author" content="">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>ARTIST APP</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700" rel="stylesheet">
+
+    <!--CSS============================================= -->
+
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/themify-icons.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+
+</head>
+<body>
+    <!-- Start header section -->
+    <header class="header-area" id="header-area">
+        <div class="dope-nav-container breakpoint-off">
+            <div class="container">
+                <div class="row">
+                    <!-- dope Menu -->
+                    <nav class="dope-navbar justify-content-between" id="dopeNav">
+
+                        <!-- Logo -->
+                        <a class="nav-brand" href="/">
+                            <img src="../img/logo1.png" alt="">
+                        </a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="dope-navbar-toggler">
+                            <span class="navbarToggler">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="dope-menu">
+
+                            <!-- close btn -->
+                            <div class="dopecloseIcon">
+                                <div class="cross-wrap">
+                                    <span class="top"></span>
+                                    <span class="bottom"></span>
+                                </div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="dopenav">
+                                <ul id="nav">
+                                    <li>
+                                        <a href="/">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="gallery">Gallery</a>
+                                    </li>
+                                    <li>
+                                        <a href="about">About</a>
+                                    </li>
+                                   
+
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+   
+
+</body>
+@yield('content')
